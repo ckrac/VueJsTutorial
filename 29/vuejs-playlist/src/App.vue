41662e617145838ae3970e1a096c0cf094c6c332
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2018-09-06 13:51:14 -0400

    initial vue init commit

---
+++ 29/vuejs-playlist/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold'
+}
+</style>
