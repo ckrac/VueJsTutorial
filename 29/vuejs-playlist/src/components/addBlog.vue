ddd6873824d1c74793368c1af96c67a1203be7ff
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2018-09-09 15:37:18 -0400

    Vue JS 2 Tutorial #43 - Posting to Firebase

--- 29/vuejs-playlist/src/components/addBlog.vue
+++ 29/vuejs-playlist/src/components/addBlog.vue
@@ -59,11 +59,7 @@ export default {
   },
   methods: {
     post() {
-      this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-        title: this.blog.title,
-        body: this.blog.content,
-        userId: 1
-      }).then((data) => {
+      this.$http.post('https://vuejs-tutorial-blog.firebaseio.com/posts.json', this.blog).then((data) => {
         console.log(data)
         this.submitted = true;
       });
